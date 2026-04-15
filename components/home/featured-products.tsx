0572010af99a5381375d8bbb89685dd629822e9a
K S Ruthvik <aruthvik4@gmail.com>
2026-04-15 17:41:43 +0530

    Added project files

--- components/home/featured-products.tsx
+++ components/home/featured-products.tsx
@@ -22,7 +22,7 @@ export function FeaturedProducts() {
           </p>
         </div>
 
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
           {featured.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
